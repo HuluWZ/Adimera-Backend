f9f3b08476783cb96a12629e19c7351d508d6a10
HuluWZ <hulunlante.w@gmail.com>
2023-12-07 22:14:55 +0300

    Add new functions to product controller

--- src/controller/product/get.product.controller.ts
+++ src/controller/product/get.product.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { getAll, showSingle } from "../../utils/db_functions/product.db";
+import {
+  getAll,
+  showSingle,
+  showRelated,
+  getCategoryProduct,
+} from "../../utils/db_functions/product.db";
 
 export const getAllProduct = async (req: Request, res: Response) => {
   const Items = await getAll();
@@ -9,5 +14,18 @@ export const getAllProduct = async (req: Request, res: Response) => {
 export const getProduct = async (req: Request, res: Response) => {
   const { id } = req.params;
   const Item = await showSingle(id);
+  res.status(200).send(Item);
+};
+
+
+export const getRelatedProduct = async (req: Request, res: Response) => {
+  const { id, category } = req.params;
+  const Item = await showRelated(id,category);
+  res.status(200).send(Item);
+};
+
+export const getProductCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const Item = await getCategoryProduct(id);
   res.status(200).send(Item);
 };
