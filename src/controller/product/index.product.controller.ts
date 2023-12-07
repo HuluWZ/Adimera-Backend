a5bca3f838f08d7b3e79a564e40b2baca8567eac
HuluWZ <hulunlante.w@gmail.com>
2023-12-07 22:15:03 +0300

    Add new functions to get related products and product categories

--- src/controller/product/index.product.controller.ts
+++ src/controller/product/index.product.controller.ts
@@ -1,5 +1,10 @@
 import { create } from "./create.product.controller"
-import { getAllProduct,getProduct } from "./get.product.controller"
+import {
+  getAllProduct,
+  getProduct,
+  getRelatedProduct,
+  getProductCategory
+} from "./get.product.controller";
 import { remove } from "./delete.product.controller"
 import { update } from "./update.product.controller"
 
@@ -8,6 +13,8 @@ export {
     create,
     getAllProduct,
     getProduct,
+    getRelatedProduct,
+    getProductCategory,
     remove,
     update
   }
